f7e6ba537bf2e57361414fe91c91ffbdc754146d
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-09-16 20:46:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
+require 'pry'
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_index do |customer, position|
+      binding.pry
+      puts "The line is currently: #{position + 1}. #{customer}"
+   end
+  end
+end
+
+
+# def take_a_number(katz_deli, name)
+#   binding.pry
+#   katz_deli.each.with_index do |position, customer|
+#     puts "position + 1" + " customer"
+#   end
+#   # Welcome, Ada. You are number 1 in line."
+#   # The line is currently: 1. Logan 2. Avi 3. Spencer"
+  
+# end
