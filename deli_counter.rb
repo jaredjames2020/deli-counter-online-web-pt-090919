33f10a67fa0614de4eeb4abe0ecb7b91c4e49d85
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-09-16 20:56:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     p katz_deli
-    #katz_deli.each_index do |customer, position|
+    katz_deli.each
      binding.pry
       puts "The line is currently: #{position + 1}. #{customer}"
    end
