4b158700c594205620259a5904b0f33eff88d9ba
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-09-16 22:10:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,18 +17,12 @@ end
 def take_a_number(katz_deli, name)
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
-    #binding.pry
 end
 
 def now_serving(katz_deli)
-  #line = []
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else 
-    # katz_deli.each do |customer| 
-    #     line << customer
-        #binding.pry
-        puts "Currently serving #{katz_deli.shift}."
-      #end
-    end
+    puts "Currently serving #{katz_deli.shift}."
+  end
 end
